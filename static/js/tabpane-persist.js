65e2ec1ab2b54097648b4af58d6bcb339b0c45c4
Patrice Chalin <pchalin@gmail.com>
2023-07-06 10:25:21 -0400

    tabpane-persist.js: tdPersistCssSelector()

--- static/js/tabpane-persist.js
+++ static/js/tabpane-persist.js
@@ -1,10 +1,14 @@
-// Storage key name also used as a data-* attribute suffix:
-const storageKeyName = 'td-tp-persist';
+const td_persistStorageKeyName = 'td-tp-persist';
+const td_persistDataAttrName = `data-${td_persistStorageKeyName}`;
+
+const tdPersistCssSelector = (attrValue) => attrValue
+  ? `[${td_persistDataAttrName}="${attrValue}"]`
+  : `[${td_persistDataAttrName}]`;
 
 function tdActivateTabsWithKey(key) {
   if (!key) return;
   document
-    .querySelectorAll(`[data-${storageKeyName}="${key}"]`)
+    .querySelectorAll(tdPersistCssSelector(key))
     .forEach((element) => {
       new bootstrap.Tab(element).show();
     });
@@ -14,7 +18,7 @@ function tdPersistActiveTab(activeTabKey) {
   if (!tdSupportsLocalStorage()) return;
 
   try {
-    localStorage.setItem(storageKeyName, activeTabKey);
+    localStorage.setItem(td_persistStorageKeyName, activeTabKey);
     tdActivateTabsWithKey(activeTabKey);
   } catch (error) {
     console.error(`Unable to save active tab '${activeTabKey}' to localStorage:`, error);
@@ -25,6 +29,6 @@ const tdSupportsLocalStorage = () => typeof Storage !== 'undefined';
 
 // On page load, activate tabs
 if (tdSupportsLocalStorage()) {
-  const activeTabKey = localStorage.getItem(storageKeyName);
+  const activeTabKey = localStorage.getItem(td_persistStorageKeyName);
   tdActivateTabsWithKey(activeTabKey);
 }
