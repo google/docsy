e9eca0fcb3b5a64929537df08abb9137cafaa51a
Patrice Chalin <chalin@users.noreply.github.com>
2024-04-26 12:12:15 -0400

    Upgrade to Hugo 0.125.4 (#1950)

--- tools/install-hugo.sh
+++ tools/install-hugo.sh
@@ -6,7 +6,11 @@ set -e
 
 PKG_JSON=${1:-package.json}
 
-if ! npm ls hugo-extended; then
-  _HUGO_EXTENDED_PKG=`perl -ne 'print "$1\@$2" if /"(hugo-extended)":\s*"\D*(.+?)"/' $PKG_JSON`
-  (set -x && npm install --save-exact -D $_HUGO_EXTENDED_PKG --omit=optional)
+if ! npm ls hugo-extended2; then
+  _HUGO_EXTENDED_VERS=`perl -ne 'print "$1" if /"hugo-extended":\s*"\D*(.+?)"/' $PKG_JSON`
+  set -x
+  if ! npm install --save-exact -D hugo-extended@$_HUGO_EXTENDED_VERS --omit=optional; then
+    echo "Trying fork instead:"
+    npm install --save-exact -D chalin/hugo-extended#v$_HUGO_EXTENDED_VERS --omit=optional
+  fi
 fi
