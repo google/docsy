9feec2fe1cc0670e7d4b07111bbf184c8353cf30
Andreas Deininger <andreas@deininger.net>
2024-07-29 21:23:32 +0200

    Incorporate reviewer feedback

--- assets/js/markmap.js
+++ assets/js/markmap.js
@@ -1,9 +1,3 @@
-{{ if lt hugo.Version "0.93.0" -}}
-    $('.language-markmap').parent().replaceWith(function() {
-        return $('<pre class="markmap">').text($(this).text());
-    });
-{{ end -}}
-
 window.markmap = {
   autoLoader: {
       manual: false,
