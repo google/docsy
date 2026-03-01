500ee239c4589f6a28643ab7f80fd161b945ecbf
Patrice Chalin <chalin@users.noreply.github.com>
2026-02-28 20:12:21 -0500

    Update version+variant menu, add siteGetPage shortcode, add tdVersion struct, and more (#2586)

--- scripts/set-package-version/hugo-yaml.test.mjs
+++ scripts/set-package-version/hugo-yaml.test.mjs
@@ -3,129 +3,140 @@ import assert from 'node:assert/strict';
 
 import { parseParamsVersion, updateYamlWithVersions } from './hugo-yaml.mjs';
 
-const fixture_basic = `
-version: 0.14.3-dev
-tdBuildId: 001-over-main-24e96f1c
+// cSpell:ignore pagelinks
+
+const fixtureBasic = `
+  latest: &tdLatestVers v0.14.3
+  dev: v0.14.4-dev # tdDevVers
+  buildId: 018-over-main-adb0e595 # tdBuildId
 `;
 
-test('parseParamsVersion extracts version and tdBuildId', () => {
-  const result = parseParamsVersion(fixture_basic);
-  assert.equal(result.version, '0.14.3-dev');
-  assert.equal(result.tdBuildId, '001-over-main-24e96f1c');
+test('parseParamsVersion extracts version info', () => {
+  const result = parseParamsVersion(fixtureBasic);
+  assert.equal(result.latest, 'v0.14.3');
+  assert.equal(result.dev, 'v0.14.4-dev');
+  assert.equal(result.buildId, '018-over-main-adb0e595');
 });
 
 const expectedVers_basic = `
-version: 0.14.4
-tdBuildId: release-abc
+  latest: &tdLatestVers v0.14.4
+  dev: v0.14.5-dev-abc # tdDevVers
+  buildId: some-build-id # tdBuildId
 `;
 
-test('updateYamlWithVersions updates version and tdBuildId in content', () => {
-  const updated = updateYamlWithVersions(fixture_basic, {
-    version: '0.14.4',
-    tdBuildId: 'release-abc',
+test('updateYamlWithVersions updates version info in content', () => {
+  const updated = updateYamlWithVersions(fixtureBasic, {
+    latest: 'v0.14.4',
+    dev: 'v0.14.5-dev-abc',
+    buildId: 'some-build-id',
   });
   assert.equal(updated, expectedVers_basic);
 });
 
-const fixtureFieldAliases = `params:
-  version: &docsyVersion 0.14.3-dev
-  tdBuildId: 001-over-main-24e96f1c
+const fixtureParams = `params:
+  tdVersion:
+    latest: &tdLatestVers v0.14.3 # TODO: ...
+    dev: &tdDevVers v0.14.4-dev
+    buildId: &tdBuildId 018-over-main-adb0e595
 `;
 
-test('parseParamsVersion extracts version and tdBuildId from field aliases', () => {
-  const result = parseParamsVersion(fixtureFieldAliases);
-  assert.equal(result.version, '0.14.3-dev');
-  assert.equal(result.tdBuildId, '001-over-main-24e96f1c');
+test('parseParamsVersion extracts version info from params section', () => {
+  const result = parseParamsVersion(fixtureParams);
+  assert.equal(result.latest, 'v0.14.3');
+  assert.equal(result.dev, 'v0.14.4-dev');
+  assert.equal(result.buildId, '018-over-main-adb0e595');
 });
 
-const expectedVersWithAlias = `params:
-  version: &docsyVersion 0.14.4
-  tdBuildId: ''
+const expectedVersParams = `params:
+  tdVersion:
+    latest: &tdLatestVers v0.14.4 # TODO: ...
+    dev: &tdDevVers v0.14.5-dev
+    buildId: &tdBuildId ''
 `;
 
-test('updateYamlWithVersions updates version and tdBuildId in content with field aliases', () => {
-  const updated = updateYamlWithVersions(fixtureFieldAliases, {
-    version: '0.14.4',
-    tdBuildId: '',
+test('updateYamlWithVersions updates version info in content with params section', () => {
+  const updated = updateYamlWithVersions(fixtureParams, {
+    latest: 'v0.14.4',
+    dev: 'v0.14.5-dev',
+    buildId: '',
   });
-  assert.equal(updated, expectedVersWithAlias);
+  assert.equal(updated, expectedVersParams);
 });
 
-const fixture_versionWithComments = `params:
-  version: 0.14.3-dev # version comment
-  tdBuildId: 001-over-main-24e96f1c # build id comment
+const expectedVersWhenNoDevOrBuildId = `
+  latest: &tdLatestVers v0.14.4
+  dev: v0.14.5-dev # tdDevVers
+  buildId: '' # tdBuildId
 `;
 
-test('parseParamsVersion extracts version and tdBuildId from content with comments', () => {
-  const result = parseParamsVersion(fixture_versionWithComments);
-  assert.equal(result.version, '0.14.3-dev');
-  assert.equal(result.tdBuildId, '001-over-main-24e96f1c');
-});
-
-const expectedVersWithComments = `params:
-  version: 0.14.4 # version comment
-  tdBuildId: 1 # build id comment
-`;
-
-test('updateYamlWithVersions updates version and tdBuildId in content with comments', () => {
-  const updated = updateYamlWithVersions(fixture_versionWithComments, {
-    version: '0.14.4',
-    tdBuildId: '1',
+test('parseParamsVersion extracts version info w/o dev or buildId', () => {
+  const updated = updateYamlWithVersions(fixtureBasic, {
+    latest: 'v0.14.4',
   });
-  assert.equal(updated, expectedVersWithComments);
+  assert.equal(updated, expectedVersWhenNoDevOrBuildId);
 });
 
 const fixture_versionWithMenuConfig = `
-version: &docsyVersion 0.14.3-dev
-tdBuildId: 001-over-main-24e96f1c # TODO: ...
-version_menu: *docsyVersion
+tdVersion:
+  latest: &tdLatestVers v0.14.3
+  dev: &tdDevVers v0.14.4-dev
+  buildId: &tdBuildId 018-over-main-adb0e595
+
+version: *tdDevVers
+version_menu: *tdDevVers
 versions:
-  - version: v0.14.2
+  - name: '**Versions**'
+  - version: *tdDevVers
+    # kind: next
+    pagelinks: true
+    url: https://main--docsydocs.netlify.app # TODO: use next.docsy.dev once available
+  - version: *tdLatestVers
+    kind: latest
+    pagelinks: true
     url: https://www.docsy.dev
-  - version: *docsyVersion
-    # url: https://next.docsy.dev
-    url: https://main--docsydocs.netlify.app/
 `;
 
-test('parseParamsVersion extracts version and tdBuildId from content with anchor', () => {
+test('parseParamsVersion extracts version info even with other fields', () => {
   const result = parseParamsVersion(fixture_versionWithMenuConfig);
-  assert.equal(result.version, '0.14.3-dev');
-  assert.equal(result.tdBuildId, '001-over-main-24e96f1c');
+  assert.equal(result.latest, 'v0.14.3');
+  assert.equal(result.dev, 'v0.14.4-dev');
+  assert.equal(result.buildId, '018-over-main-adb0e595');
 });
 
 const expectedVersWithMenuConfig = `
-version: &docsyVersion 0.15.0-dev
-tdBuildId: 001-over-main-24e96f1c # TODO: ...
-version_menu: *docsyVersion
+tdVersion:
+  latest: &tdLatestVers v0.14.4
+  dev: &tdDevVers v0.14.5-dev
+  buildId: &tdBuildId ''
+
+version: *tdDevVers
+version_menu: *tdDevVers
 versions:
-  - version: v0.14.2
+  - name: '**Versions**'
+  - version: *tdDevVers
+    # kind: next
+    pagelinks: true
+    url: https://main--docsydocs.netlify.app # TODO: use next.docsy.dev once available
+  - version: *tdLatestVers
+    kind: latest
+    pagelinks: true
     url: https://www.docsy.dev
-  - version: *docsyVersion
-    # url: https://next.docsy.dev
-    url: https://main--docsydocs.netlify.app/
 `;
 
-test('updateYamlWithVersions updates version and tdBuildId in content with menu config', () => {
+test('updateYamlWithVersions updates version info in content with menu config', () => {
   const updated = updateYamlWithVersions(fixture_versionWithMenuConfig, {
-    version: '0.15.0-dev',
-    tdBuildId: '001-over-main-24e96f1c',
+    latest: 'v0.14.4',
   });
   assert.equal(updated, expectedVersWithMenuConfig);
 });
 
 const fixture_versionQuoted = `
-version: '0.14.3-dev'
-tdBuildId: "001-over-main-24e96f1c"
+  latest: 'v0.14.4' # tdLatestVers
+  buildId: '1' # tdBuildId
 `;
 
-test('parseParamsVersion extracts version and tdBuildId from content with quoted values', () => {
+test('parseParamsVersion extracts version info with quoted latest', () => {
   const result = parseParamsVersion(fixture_versionQuoted);
-  assert.equal(result.version, '0.14.3-dev');
-  assert.equal(result.tdBuildId, '001-over-main-24e96f1c');
-});
-
-test('parseParamsVersion extracts empty string for tdBuildId: ""', () => {
-  const result = parseParamsVersion("version: 0.14.0\ntdBuildId: ''");
-  assert.equal(result.version, '0.14.0');
-  assert.equal(result.tdBuildId, '');
+  assert.equal(result.latest, 'v0.14.4');
+  assert.equal(result.buildId, '1');
 });
